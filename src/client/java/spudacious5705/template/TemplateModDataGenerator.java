19b223018f7cd1240ca7bae269449af3a3b7249b
Spudacious <145444678+Milo-Cat@users.noreply.github.com>
2025-04-01 22:27:20 +0100

    Initial commit

---
+++ src/client/java/spudacious5705/template/TemplateModDataGenerator.java
@@ -0,0 +1,11 @@
+package spudacious5705.template;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
